6c7d550d1ac1b79bd15b6dc4d1b82e01f4db28c9
Gerald Villorente <gerald@getpantheon.com>
2019-04-02 15:51:14 +0800

    Contenta codebase

---
+++ web/modules/contrib/consumer_image_styles/src/ConsumerImageStylesServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\consumer_image_styles;
+
+use Drupal\Core\DependencyInjection\ContainerBuilder;
+use Drupal\Core\DependencyInjection\ServiceProviderBase;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Replace the resource type repository for our own configurable version.
+ */
+class ConsumerImageStylesServiceProvider extends ServiceProviderBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function alter(ContainerBuilder $container) {
+    if ($container->has('serializer.normalizer.link_collection.jsonapi')) {
+      $container->getDefinition('serializer.normalizer.link_collection.jsonapi')->clearTags();
+    }
+  }
+
+}
